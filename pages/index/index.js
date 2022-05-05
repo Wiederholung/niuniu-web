66e16affbcdb728ad712b3e60704ac2ed7706927
Berqio <1419086503@qq.com>
2022-05-06 01:54:57 +0800

    首页更改滚动

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,15 @@
 // index.js
-Page({})
+Page({
+    data: {
+        imgUrls: [
+            '../../images/icon/home.png',
+            '../../images/icon/home.png',
+            '../../images/code-cloud-callback-config.png'
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1200
+    }
+})
