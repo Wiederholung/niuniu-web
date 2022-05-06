5c379e1adc65845ad2479db6139d129210121ce1
Berqio <1419086503@qq.com>
2022-05-06 10:36:46 +0800

    首页六模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,52 @@ Page({
             '../../images/icon/home.png',
             '../../images/code-cloud-callback-config.png'
         ],
-        indicatorDots: true,
-        vertical: false,
-        autoplay: true,
-        interval: 3000,
-        duration: 1200
-    }
+        // swiperList:[] //存放轮播图数据
+        iconArray: [
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": '自学路径推荐'
+            },
+            
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": 'Bry博客导航'
+            },
+            
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": '科研网站导航'
+            },
+            
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": '常用工具教学'
+            },
+            
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": '课内资源'
+            },
+            
+            {
+                "iconUrl": '../../images/icon/black-board-f.png',
+                "iconText": '发现'
+            }
+        ]
+    },
+    onload:function(){
+        // this.getSwiperList()
+    },
+    // getSwiperList(){//获取轮播图数据的方法
+    //     wx.request({
+    //       url: 'https://wwww.escook.cn/slides',
+    //       method:'GET',
+    //       success:(res)=>{
+    //         console.log(res)
+    //         this.setData({
+    //             swiperList:res.data
+    //         })
+    //       }
+    //     })
+    // }
 })
